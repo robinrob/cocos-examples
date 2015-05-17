d92dace7d231e2a6674b796f1b90b48e322f8108
Robin Smith <robin@mrrobinsmith.com>
2015-05-17 22:03:41 +0100

    Must use renderMode 1!!!

--- src/examples/ExampleDraw.js
+++ src/examples/ExampleDraw.js
@@ -14,34 +14,26 @@ var ExampleDraw = {
             this.addChild(draw)
 
             var width = 100
-
             var pos = cc.p(-200, 200)
             var square = rss.squareVerts(width)
             square = rss.p.addAll(square, pos)
-            draw.drawPoly(square, rss.colors.green, 1, rss.colors.green)
+            draw.drawPoly(square, rss.colors.green, 5, rss.colors.blue)
 
             pos = cc.p(+200, 200)
             var square45 = rss.squareVerts(width)
             square45 = rss.rotateAll(square45, rss.toRad(45))
             square45 = rss.p.addAll(square45, pos)
-            draw.drawPoly(square45, rss.colors.red, 1, rss.colors.red)
+            draw.drawPoly(square45, rss.colors.maroon, 5, rss.colors.red)
 
-            //pos = cc.p(-200, -200)
-            //var star4 = rss.starVerts(4, width, width * 0.5, width * 0.2)
-            //star4 = rss.p.addAll(star4, pos)
-            //draw.drawPoly(star4, rss.colors.yellow, 1, rss.colors.yellow)
+            pos = cc.p(-200, -200)
+            var star4 = rss.starVerts(5, width, width * 0.5, width * 0.2)
+            star4 = rss.p.addAll(star4, pos)
+            draw.drawPoly(star4, rss.colors.yellow, 5, rss.colors.red)
 
-            var star = Star.create({pos: rss.center()})
-            star.setColor(rss.colors.yellow)
-            star.draw()
-            this.addChild(star)
-
-            //pos = cc.p()
-            //var circSeg = rss.circSegmentVerts(width, 0, 0, 50)
-            //circSeg = rss.p.addAll(circSeg, pos)
-            //draw.drawPoly(circSeg, rss.colors.yellow, 1, rss.colors.yellow)
-
-
+            pos = cc.p(+200, -200)
+            var circSeg = rss.circSegmentVerts(width, rss.toRad(45), 0, 50)
+            circSeg = rss.p.addAll(circSeg, pos)
+            draw.drawPoly(circSeg, rss.colors.purple, 5, rss.colors.yellow)
         }
     }),
 
