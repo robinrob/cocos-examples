5870a208b9de37693b6d92918d944f3d356960f3
Robin Smith <robin@mrrobinsmith.com>
2015-05-18 14:55:10 +0100

    Update.

--- refucktor2.zsh
+++ refucktor2.zsh
@@ -4,11 +4,11 @@
 autoload -U +X $fpath[1]/*(:t) 2> /dev/null
 source $ZSHCOLORS_PATH
 
-sed_all 's/rss\.add(?=\b)/rss\.p\.add/g' src '*.js'
+sed_all 's/rss\.add\(?=\b\)/rss\.p\.add/g' src '*.js'
 sed_all 's/rss\.addX/rss\.p\.addX/g' src '*.js'
 sed_all 's/rss\.addY/rss\.p\.addY/g' src '*.js'
-sed_all 's/rss\.sub(?=\b)/rss\.p\.sub/g' src '*.js'
-sed_all 's/rss\.mult(?=b)/rss\.p\.mult/g' src '*.js'
+sed_all 's/rss\.sub\(?=\b\)/rss\.p\.sub/g' src '*.js'
+sed_all 's/rss\.mult\(?=b\)/rss\.p\.mult/g' src '*.js'
 
 sed_all 's/rss\.p.addW/rss\.s\.addW/g' src '*.js'
 sed_all 's/rss\.p.addH/rss\.s\.addH/g' src '*.js'
