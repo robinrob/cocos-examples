bd50ea5cad01077159669d9d61bb9391ec100295
Robin Smith <robin@mrrobinsmith.com>
2015-05-18 14:46:22 +0100

    Refactor script 2.

---
+++ refucktor2.sh
@@ -0,0 +1,14 @@
+sal 's/rss\.add(?=\b)/rss\.p\.add/g' src '*.js'
+sal 's/rss\.addX/rss\.p\.addX/g' src '*.js'
+sal 's/rss\.addY/rss\.p\.addY/g' src '*.js'
+sal 's/rss\.sub(?=\b)/rss\.p\.sub/g' src '*.js'
+sal 's/rss\.mult(?=b)/rss\.p\.mult/g' src '*.js'
+
+sal 's/rss\.p.addW/rss\.s\.addW/g' src '*.js'
+sal 's/rss\.p.addH/rss\.s\.addH/g' src '*.js'
+
+sal 's/rss\.addW/rss\.s\.addW/g' src '*.js'
+sal 's/rss\.addH/rss\.s\.addH/g' src '*.js'
+sal 's/rss\.subW/rss\.s\.subW/g' src '*.js'
+sal 's/rss\.subH/rss\.s\.subH/g' src '*.js'
+sal 's/rss\.multS/rss\.s\.mult/g' src '*.js'
